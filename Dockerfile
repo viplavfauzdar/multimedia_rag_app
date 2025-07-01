4153a07dfe3467e8720cee3c8ff0ffccaaea9d08
viplav.fauzdar <viplav.fauzdar@gmail.com>
2025-07-01 15:59:47 -0400

    updates for EC2

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ RUN apt-get update && apt-get install -y \
     git \
     tesseract-ocr \
     ffmpeg \
-    libsm6 \
-    libxext6 \
+#    libsm6 \
+#    libxext6 \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,4 @@
 docker run -p 8501:8501 --env-file .env multimedia-rag-app
+#For running on server
+#echo "OPENAI_API_KEY=your_actual_api_key" > .env
+sudo docker run -p 80:8501 -e OPENAI_API_KEY=your_actual_api_key_here multimedia-rag-app
